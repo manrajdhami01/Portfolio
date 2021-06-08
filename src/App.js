63b4dc7b0d401d0fc741b5c1cf18308e39551650
manrajdhami01 <manrajdhami007@gmail.com>
2021-06-08 00:17:49 -0400

    Header and Background finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Particles from "react-particles-js";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#0ca21d",
+              },
+            },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
+  );
 }
 
 export default App;
